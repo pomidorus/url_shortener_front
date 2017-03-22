b3e2db79e9668c0993de42a1a3a6d36941f32eb2
Andrey Seleznov <andrewaka39@gmail.com>
2017-03-22 21:12:35 +0000

    add url_bar component

---
+++ src/url_bar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UrlBar extends Component {
+  render () {
+    return (
+      <div className="url-bar">
+        <input/>
+        <button>Short it!</button>
+      </div>
+    );
+  }
+}
+
+export default UrlBar;
